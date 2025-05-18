83b55f89ea19837cd6d5e7f85bc237fde25b566f
calebmaika <guintocaleb@gmail.com>
2025-05-18 22:51:53 +0800

    change backend route
    groupg-fullstack-app-production-31db.up.railway.app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://groupg-fullstack-app-production.up.railway.app'
+    apiUrl: 'groupg-fullstack-app-production-31db.up.railway.app'
 }
