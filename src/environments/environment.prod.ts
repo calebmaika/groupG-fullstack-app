bf0217be034432301b974a84356e254e8c12c1c8
calebmaika <guintocaleb@gmail.com>
2025-05-18 23:03:25 +0800

    atay wa napud nabutngan ug htps

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'groupg-fullstack-app-production-31db.up.railway.app'
+    apiUrl: 'https://groupg-fullstack-app-production-31db.up.railway.app'
 }
